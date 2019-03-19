38d32972ac6a4ecdf8375e11e0f5fce8c098ba1c
Jenn <jenn.a.martinez@outlook.com>
2019-03-19 15:03:00 -0700

    readme edits

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,33 +5,32 @@ var path = require("path");
 var participants = require("../data/friends.js");
 
 // GET and POST Routes //
-module.exports = function(app) {
-app.get("/api/friends", function(req, res) {
-    res.json(participants);
-});
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(participants);
+    });
 
-// Determines results from the survery based on the request from user response //
-app.post("/api/friends", function(req, res) {
-    var friendMatch = { name: "", photo: "", comparison: 100 }
-    var userAnswers = req.body;
-    console.log(req.body);
-    var totalDifference = 0;
-    
-    for (var i = 0; i < participants.length; i++) {
-        totalDifference = 0;
-        for (var j = 0; j < participants[i].scores.length; j++) {
-            var scoring = Math.abs(userAnswers.scores[j] - participants[i].scores[j]);
-            totalDifference += scoring;
+    // Determines results from the survery based on the request from user response //
+    app.post("/api/friends", function (req, res) {
+        var friendMatch = { name: "", photo: "", comparison: 100 }
+        var userAnswers = req.body;
+        console.log(req.body);
+        var totalDifference = 0;
 
-            if (totalDifference <= friendMatch.comparison) {
-                friendMatch.name = participants[i].name,
-                friendMatch.photo = participants[i].photo,
-                friendMatch.comparison = totalDifference 
+        for (var i = 0; i < participants.length; i++) {
+            totalDifference = 0;
+            for (var j = 0; j < participants[i].scores.length; j++) {
+                var scoring = Math.abs(userAnswers.scores[j] - participants[i].scores[j]);
+                totalDifference += scoring;
+
+                if (totalDifference <= friendMatch.comparison) {
+                    friendMatch.name = participants[i].name,
+                        friendMatch.photo = participants[i].photo,
+                        friendMatch.comparison = totalDifference
+                }
             }
         }
-    }    
-res.json(friendMatch);
-participants.push(userAnswers);
-    
-  });
+        res.json(friendMatch);
+        participants.push(userAnswers);
+    });
 };
